6abfdc984a774c02424545ee2460f54d4cce7785
Suarez <jazmisu.arez.js@gmail.com>
2024-08-20 17:59:20 +0100

    (genshin) character class turned into child class of Character

--- teyvatdle.py
+++ teyvatdle.py
@@ -5,31 +5,29 @@ from PIL import Image, ImageDraw
 
 
 class Character:
-    attributes = ["name", "nation", "height", "vision", "weapon", "version"]
+    attributes = ["name", "version"]
     
-    def __init__(self, ID, name, height, vision, weapon, nation, version):
+    def __init__(self, ID, name, version):
         self.ID = ID
         self.name = name
-        self.nation = nation
-        self.height = height
-        self.vision = vision
-        self.weapon = weapon
         self.version = version
 
     def compareTo(self, guess):
-        common = {key: 1 for key in __class__.attributes}
+        common = {key: 1 for key in self.attributes}
         self_dict, guess_dict = vars(self), vars(guess)
 
         for attribute in common:
             common[attribute] = self_dict[attribute] == guess_dict[attribute]
         else:
-            if not common["height"] and \
-            "Medium" in (self_dict["height"], guess_dict["height"]):
-                common["height"] = 0.5
             if not common["version"] and \
             self_dict["version"][0] == guess_dict["version"][0]:
                 common["version"] = 0.5
         
+        return common
+    
+    def newCompareImage(self, guess, common):
+        guess_dict = vars(guess)
+
         cards = dict()
         size = (256, 256)
         for attr in common:
@@ -41,13 +39,9 @@ class Character:
                     background = "gold"
                 case 1:
                     background = "green"
-            if attr == "height" or attr == "version":
-                strokewidth = 1 + (attr == "version")
-                textsize = 50 * strokewidth
-                c["img"] = Image.new("RGBA", size, background)
-                foreground = ImageDraw.Draw(c["img"])
-                foreground.text((size[0]/2,size[1]/2), guess_dict[attr], "white", anchor="mm", stroke_width=strokewidth, font_size=textsize)
-            else:
+
+            c["img"] = self.textCard(attr, c, size, background, guess)
+            if not c["img"]:
                 c["foreground"] = Image.open(f"icons/{guess_dict[attr].replace(" ","_")}_Icon.png").convert('RGBA')
                 c["background"] = Image.new("RGBA", size, background)
                 c["img"] = Image.alpha_composite(c["background"], c["foreground"])
@@ -61,12 +55,28 @@ class Character:
         image = Image.new("RGBA", fullsize)
         texts = ImageDraw.Draw(image)
         # texts.text((fullsize[0]//2, fullsize[1]//4), guess.name, fill=(30, 20, 10), anchor="mm", stroke_width=20, stroke_fill=(96, 64, 48), font_size=150)
-        texts.text((fullsize[0]//2, fullsize[1]//4), guess.name, fill=(240, 220, 200), anchor="mm", stroke_width=20, stroke_fill=(96, 64, 48), font_size=150)
+        texts.text(
+            (fullsize[0]//2, fullsize[1]//4),
+            guess.name,
+            fill=(240, 220, 200),
+            anchor="mm",
+            stroke_width=20,
+            stroke_fill=(96, 64, 48),
+            font_size=150
+        )
         texts.rectangle([(c_x,title_y-c_sep*1.25),((len(cards)*(size[0]+c_sep))-c_sep-1,title_y+c_sep*1.25)], "white")
 
         for c in cards:
             if c != "name":
-                texts.text(((c_x + size[0]//2), title_y), c, fill=(96, 64, 48), anchor="mm", stroke_width=2, stroke_fill=(96, 64, 48), font_size=34)
+                texts.text(
+                    ((c_x + size[0]//2), title_y),
+                    c,
+                    fill=(96, 64, 48),
+                    anchor="mm",
+                    stroke_width=2,
+                    stroke_fill=(96, 64, 48),
+                    font_size=34
+                )
 
             card = cards[c]["img"]
             image.paste(card, (c_x, c_y), card.convert('RGBA'))
@@ -74,7 +84,28 @@ class Character:
 
         # image.show()
         image.save(f"temp/tvd_guess.png", "PNG")
-        return common
+
+    @staticmethod
+    def textCard(attr, card, size, background, guess):
+        guess_dict = vars(guess)
+
+        if attr == "version":
+            strokewidth = 2
+            textsize = 50 * strokewidth
+
+            card["img"] = Image.new("RGBA", size, background)
+            foreground = ImageDraw.Draw(card["img"])
+            foreground.text(
+                (size[0]/2,size[1]/2),
+                guess_dict[attr],
+                "white",
+                anchor="mm",
+                stroke_width=strokewidth,
+                font_size=textsize
+            )
+            return card["img"]
+
+        return False
 
     def __repr__(self):
         return f"({self.ID:0>3}) {self.name}"
@@ -100,6 +131,51 @@ class Character:
 
         if matches == 1:
             return fullname
+        return False
+    
+
+class GenshinChar(Character):
+    attributes = Character.attributes + ["nation", "height", "vision", "weapon"]
+    attributes.append(attributes.pop(1)) # Move the version attribute to the end
+
+    def __init__(self, ID, name, height, vision, weapon, nation, version):
+        super().__init__(ID, name, version)
+        self.nation = nation
+        self.height = height
+        self.vision = vision
+        self.weapon = weapon
+
+    def compareTo(self, guess):
+        self_dict, guess_dict = vars(self), vars(guess)
+
+        common = super().compareTo(guess)
+        if not common["height"] and \
+        "Medium" in (self_dict["height"], guess_dict["height"]):
+            common["height"] = 0.5
+
+        self.newCompareImage(guess, common)
+        return common
+
+    @staticmethod
+    def textCard(attr, card, size, background, guess):
+        guess_dict = vars(guess)
+
+        if attr == "height" or attr == "version":
+            strokewidth = 1 + (attr == "version")
+            textsize = 50 * strokewidth
+
+            card["img"] = Image.new("RGBA", size, background)
+            foreground = ImageDraw.Draw(card["img"])
+            foreground.text(
+                (size[0]/2,size[1]/2),
+                guess_dict[attr],
+                "white",
+                anchor="mm",
+                stroke_width=strokewidth,
+                font_size=textsize
+            )
+            return card["img"]
+
         return False
 
 
@@ -128,7 +204,7 @@ def getCharacters(**conditions):
         keys = "name LIKE ?"
         values = ['%']
         for key in conditions:
-            if key in Character.attributes:
+            if key in GenshinChar.attributes:
                 keys += f" AND {key} = ?"
                 values.append(conditions[key].title())
 
@@ -138,7 +214,7 @@ def getCharacters(**conditions):
             WHERE {keys}
             """, tuple(values))
                 
-        return [Character(*attrs) for attrs in c.fetchall()]
+        return [GenshinChar(*attrs) for attrs in c.fetchall()]
     finally:    
         c.close()
         con.close()
@@ -155,8 +231,6 @@ def getNames():
 
 
 if __name__ == "__main__":
-
-    # print(char, guess, char.compareTo(guess), sep="\n")
 
     # for name in [
     #     "htu aop",
@@ -214,4 +288,4 @@ if __name__ == "__main__":
     # ]:
     #     print(name, typoguesser.guessFrom(getNames(), name, max_missing=2), sep=": ")
 
-    print(Character.random().compareTo(Character.random()))
+    print(GenshinChar.random().compareTo(GenshinChar.random()))
